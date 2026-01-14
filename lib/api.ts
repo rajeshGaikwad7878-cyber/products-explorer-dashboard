15d1814b89ab35bf50eb53672e205e76b0e3b7c2
Rajesh Gaikwad <rajeshgaikwad744@gmail.com>
2026-01-14 12:29:55 +0530

    fetch api issue for prod resolved

--- lib/api.ts
+++ lib/api.ts
@@ -4,9 +4,7 @@ const BASE_URL = "https://fakestoreapi.com";
 
 export async function fetchProducts(): Promise<Product[]> {
   try {
-    const res = await fetch(`${BASE_URL}/products`, {
-      cache: "no-store",
-    });
+    const res = await fetch(`${BASE_URL}/products`);
 
     if (!res.ok) {
       throw new Error("Failed to fetch products");
